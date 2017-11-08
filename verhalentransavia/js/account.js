97f0a76ca5a48b296991dc6d032fbe8cd7117bfc
sterrevangeest <32976396+sterrevangeest@users.noreply.github.com>
2017-11-08 21:48:24 +0100

    Add files via upload

---
+++ verhalentransavia/js/account.js
@@ -0,0 +1,100 @@
+//test
+console.log("hello");
+
+// microinteraction 2
+    var button = document.querySelector("header section:nth-of-type(3)")
+    var accountOptions = document.querySelector(".offcanvas-account")
+    var page = document.querySelector("main")
+
+    var accountSwitchUser1 = document.querySelector('.offcanvas-account div:nth-of-type(1) #user-1')
+    var accountSwitchUser2 = document.querySelector('.offcanvas-account div:nth-of-type(1) #user-2')
+    var accountSwitchUser3 = document.querySelector('.offcanvas-account div:nth-of-type(1) #user-3')
+    var accountSwitchUser4 = document.querySelector('.offcanvas-account div:nth-of-type(1) #user-4')
+
+
+    console.log(button);
+    console.log(accountOptions);
+
+    button.addEventListener('click', function () {
+        //class op header section:last-child
+        accountOptions.classList.toggle('active')
+        page.classList.toggle("pageDown")
+        button.classList.add("active")
+    });
+
+
+    accountSwitchUser1.addEventListener('click', function () {
+        accountSwitchUser1.classList.toggle('active')
+        //notactive
+        accountSwitchUser2.classList.remove('active')
+        accountSwitchUser3.classList.remove('active')
+        accountSwitchUser4.classList.remove('active')
+
+         //colorchange userbutton
+        button.classList.toggle('user-1')
+
+        button.classList.remove('user-2')
+        button.classList.remove('user-4')
+        button.classList.remove('user-3')
+    });
+
+    accountSwitchUser2.addEventListener('click', function () {
+        accountSwitchUser2.classList.toggle('active')
+        //notactive
+        accountSwitchUser1.classList.remove('active')
+        accountSwitchUser3.classList.remove('active')
+        accountSwitchUser4.classList.remove('active')
+
+        //colorchange userbutton
+        button.classList.toggle('user-2')
+
+        button.classList.remove('user-1')
+        button.classList.remove('user-4')
+        button.classList.remove('user-3')
+
+    });
+
+
+    accountSwitchUser3.addEventListener('click', function () {
+        accountSwitchUser3.classList.toggle('active')
+        //notactive
+        accountSwitchUser1.classList.remove('active')
+        accountSwitchUser2.classList.remove('active')
+        accountSwitchUser4.classList.remove('active')
+
+        //colorchange userbutton
+        button.classList.toggle('user-3')
+
+        button.classList.remove('user-2')
+        button.classList.remove('user-4')
+        button.classList.remove('user-1')
+    });
+
+    accountSwitchUser4.addEventListener('click', function () {
+        accountSwitchUser4.classList.toggle('active')
+        //notactive
+        accountSwitchUser2.classList.remove('active')
+        accountSwitchUser3.classList.remove('active')
+        accountSwitchUser1.classList.remove('active')
+
+        //colorchange userbutton
+        button.classList.add('user-4')
+
+        button.classList.remove('user-2')
+        button.classList.remove('user-1')
+        button.classList.remove('user-3')
+    });
+
+
+var plusButton = document.querySelector("section article footer .save")
+var savePopup = document.querySelector ("main div:nth-of-type(1) .savePopup")
+
+plusButton.addEventListener("click", function(){
+    plusButton.classList.toggle('active');
+    savePopup.classList.toggle('active');
+
+})
+
+
+
+
